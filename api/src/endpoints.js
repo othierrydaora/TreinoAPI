a0df3f53f9fd0b39e6cf5f4d35a21933e038ab28
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-25 09:02:53 -0300

    Tratamento de erro feito

--- api/src/endpoints.js
+++ api/src/endpoints.js
@@ -6,62 +6,110 @@ const server = Router()
 
 
 server.get('/dobro/:numero', (req, resp) => {
-    resp.send(dobro(req.params.numero));
+    try {
+        resp.send(dobro(req.params.numero));        
+    } catch (err) {
+        resp.status(400).send(`Erro: ${err.message}`);
+    }
 }); //ok
 
 server.get('/somar', (req, resp) => {
-    resp.send(soma(req.query.numeroUm, req.query.numeroDois));
+    try {
+        resp.send(soma(req.query.numeroUm, req.query.numeroDois));
+    } catch (err) {
+        resp.status(400).send(`Erro: ${err.message}`);
+    }
 }); //ok
 
 server.post('/media', (req, resp) => {
-    const numUm = Number(req.body.numUm);
-    const numDois = Number(req.body.numDois);
-    const numTres = Number(req.body.numTres);
+    try {
+        const numUm = Number(req.body.numUm);
+        const numDois = Number(req.body.numDois);
+        const numTres = Number(req.body.numTres);
 
-    resp.send({
-        "media": media(numUm, numDois, numTres)
-    });
+        resp.send({
+            "media": media(numUm, numDois, numTres)
+        });
+    } catch (err) {
+        resp.status(400).send(`Erro: ${err.message}`);
+    }
 }); //ok
 
 server.get('/febre/:temperatura', (req, resp) => {
-    resp.send({
-        "febrio": febre(req.params.temperatura)
-    });
+    try {
+        resp.send({
+            "febrio": febre(req.params.temperatura)
+        });
+    } catch (err) {
+        resp.status(400).send({
+            "Erro": err.message
+        });
+    }
 }); //ok
 
 server.get('/tabuada/:limite', (req, resp) => {
-    resp.send({
-        "tabuada": table(req.params.limite)
-    });
+    try {
+        resp.send({
+            "tabuada": table(req.params.limite)
+        });
+    } catch (err) {
+        resp.status(400).send({
+            "Erro": err.message
+        });
+    }
 }); //ok
 
 server.get('/dia2/corprimaria/:cor', (req, resp) => {
-    resp.send({
-        "primaria": corPrimaria(req.params.cor)
-    });
+    try {
+        resp.send({
+            "primaria": corPrimaria(req.params.cor)
+        });
+    } catch (err) {
+        resp.status(400).send({
+            "Erro": err.message
+        });
+    }
 }); //ok
 
 
 server.post('/ingresso', (req, resp) => {
-    const { inteiras, meias, dia, origem } = req.body;
-    resp.send({
-        "total": ingresso(inteiras, meias, dia, origem)
-    });
+    try {
+        const { inteiras, meias, dia, origem } = req.body;
+        resp.send({
+            "total": ingresso(inteiras, meias, dia, origem)
+        });
+    } catch (err) {
+        resp.status(400).send({
+            "Erro": err.message
+        });
+    }
 }); //ok
 
 server.get('/dia2/freqcaractere/:texto/:caractere', (req, resp) => {
-    const texto = req.params.texto;
-    const caractere = req.params.caractere;
-    resp.send({
-        "frequencia": characterFreq(texto, caractere)
-    });
+    try {
+        const texto = req.params.texto;
+        const caractere = req.params.caractere;
+        resp.send({
+            "frequencia": characterFreq(texto, caractere)
+        });
+    } catch (err) {
+        resp.status(400).send({
+            "Erro": err.message
+        });
+    }
 }); //ok
 
 server.post('/dia2/maior', (req, resp) => {
-    const arr = req.body.numeros;
-    resp.send({
-        "maior": maior(arr)
-    });
+    try {
+        const arr = req.body.numeros;
+        resp.send({
+            "maior": maior(arr)
+        });
+    } catch (err) {
+        resp.status(400).send({
+            "Erro": err.message
+        });   
+    }
 }); //ok
 
 export default server;
